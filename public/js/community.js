bc5360f97b6f29eef2f8295691f4d511851c9239
tomurango <thisistomura@gmail.com>
2021-11-16 13:14:35 +0900

    ready for list of community

---
+++ public/js/community.js
@@ -0,0 +1,7 @@
+function com_others(){
+    document.getElementById("com_list_div").style.display = "block";
+}
+
+function com_others_back(){
+    document.getElementById("com_list_div").style.display = "none";
+}
