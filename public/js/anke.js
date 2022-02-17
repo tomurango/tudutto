340038c92ac6d3f50519fc2cf57fcc6eb6055512
tomurango <thisistomura@gmail.com>
2022-02-17 16:30:31 +0900

    20220217

--- public/js/anke.js
+++ public/js/anke.js
@@ -5,15 +5,15 @@ function tag_anke(element){
             element.classList.toggle("disactive");
             document.getElementById("anke_follow").classList.toggle("active");
             document.getElementById("anke_follow").classList.toggle("disactive");
-            //document.getElementById("danjon_look_page").style.display = "flex";
-            //document.getElementById("danjon_follow_page").style.display = "none";
+            document.getElementById("listforanke").style.display = "flex";
+            document.getElementById("divsforanke").style.display = "none";
         }else if(element.id == "anke_follow"){
             element.classList.toggle("active");
             element.classList.toggle("disactive");
             document.getElementById("anke_look").classList.toggle("active");
             document.getElementById("anke_look").classList.toggle("disactive");
-            //document.getElementById("danjon_follow_page").style.display = "block";
-            //document.getElementById("danjon_look_page").style.display = "none";
+            document.getElementById("divsforanke").style.display = "block";
+            document.getElementById("listforanke").style.display = "none";
         }
     }else{
         //activeのボタンを押したので何もしないで関数を終了させる
